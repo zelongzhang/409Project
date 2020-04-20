1902c09d212bbc3d24b93acb51833524530763a3
zelongzhang <zelong.zhang1@ucalgary.ca>
2020-04-20 00:38:30 -0600

    bandaid solution for maaz not wanting to click buttons3

--- RegistrationServer/src/RegistrationServer.java
+++ RegistrationServer/src/RegistrationServer.java
@@ -61,7 +61,7 @@ public class RegistrationServer
 	}
 	
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		RegistrationServer regServer = new RegistrationServer();
 		regServer.start();
